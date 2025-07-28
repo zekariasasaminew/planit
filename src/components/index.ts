9a15981ac8d56d2478c6602d4c4f4c7ce25bb5ed
Zekarias Asaminew <zekariassolomon1122@gmail.com>
2025-07-28 18:48:16 -0500

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { AppBar } from './AppBar';
+export { Sidebar } from './Sidebar';
+export { ThemeToggle } from './ThemeToggle';
+export { SemesterCard } from './SemesterCard';
+export { MajorSelector } from './MajorSelector';
+export { MinorSelector } from './MinorSelector';
+export { PreferenceForm } from './PreferenceForm'; 
